0b529138843e8086aa5523f0767e8ab449f12d9c
SuriyaRamesh <suriya.ram@anywhere.co>
2025-08-07 19:06:48 +0530

    build: style sheet trigger

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,12 +1,16 @@
 import { ShoppingCart } from "lucide-react";
-import { Sheet, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
 const Cart = () => {
     return (
         <Sheet>
-            <SheetTrigger className="group">
-                <ShoppingCart className="size-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
+            <SheetTrigger className="group -m-2 flex items-center p-2">
+                <ShoppingCart aria-hidden='true' className="size-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
+                <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                    0
+                </span>
             </SheetTrigger>
+            
         </Sheet>
     );
 }
